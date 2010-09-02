3b26a6d98cc4d7b0c4f65961511c04b2f590f79c
Pete Beardmore <pete.beardmore@msn.com>
2010-09-02 07:34:01 +0100

    [mod] whitespace changes only
    -supplemental to previous commit

--- svn/svn.sh
+++ svn/svn.sh
@@ -11,50 +11,50 @@ fi
 option=$1
 if [ $option = "add-repo" ]; then
 
-target=$1
-if [ "$(echo "$target" | awk '{print substr($0, length($0))}')" = "/" ]; then
-  target=$(echo "$target" | awk '{print substr($0, 1, length($0) - 1)}')
-fi
-
-repo=$(echo "$target" | sed 's/.*\/\(.*\)/\1/')
-
-if ! [ -d $target ]; then
-  mkdir -p $target
-  if [ $? -ne 0 ]; then
-    echo cannot create $target
-    exit 1
+  target=$1
+  if [ "$(echo "$target" | awk '{print substr($0, length($0))}')" = "/" ]; then
+    target=$(echo "$target" | awk '{print substr($0, 1, length($0) - 1)}')
   fi
-fi
 
-if [ "$(ls -A $target)" ]; then
-  echo -n "repo path is not empty, delete contents of $target? [y/n]"
-  declare result
-  while read -es -n 1 result ; do
-    if [ "$result" = "y" ]; then
-      break
-    elif [ "$result" = "n" ]; then
+  repo=$(echo "$target" | sed 's/.*\/\(.*\)/\1/')
+
+  if ! [ -d $target ]; then
+    mkdir -p $target
+    if [ $? -ne 0 ]; then
+      echo cannot create $target
       exit 1
     fi
-  done
-  if [ "$result" = "y" ]; then
-    rm -R $target/*
-  else
-    exit 1
   fi
-fi
 
-# cd necessary as svnadmin doesn't handle relative paths
-cwd=$(pwd)
-cd $target
-svnadmin create --fs-type fsfs $target
-chown -R $REPO_OWNER_ID:$REPO_OWNER_ID $target
-chmod -R ug+rw $target
+  if [ "$(ls -A $target)" ]; then
+    echo -n "repo path is not empty, delete contents of $target? [y/n]"
+    declare result
+    while read -es -n 1 result ; do
+      if [ "$result" = "y" ]; then
+        break
+      elif [ "$result" = "n" ]; then
+        exit 1
+      fi
+    done
+    if [ "$result" = "y" ]; then
+      rm -R $target/*
+    else
+      exit 1
+    fi
+  fi
 
-rm -rf temp
-svn co $SERVER$repo temp
-svn mkdir temp/branches temp/tags temp/trunk
-svn ci -m "[add] repository structure" ./temp
-rm -rf temp
+  # cd necessary as svnadmin doesn't handle relative paths
+  cwd=$(pwd)
+  cd $target
+  svnadmin create --fs-type fsfs $target
+  chown -R $REPO_OWNER_ID:$REPO_OWNER_ID $target
+  chmod -R ug+rw $target
+
+  rm -rf temp
+  svn co $SERVER$repo temp
+  svn mkdir temp/branches temp/tags temp/trunk
+  svn ci -m "[add] repository structure" ./temp
+  rm -rf temp
 
 elif [ $option = "clean-repo" ]; then
 
