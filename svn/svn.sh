35b00ebfac7196cfb1fc40084fedc34261ee2850
Pete Beardmore <pete.beardmore@msn.com>
2013-06-08 11:28:35 +0100

    [mod] whitespace changes only
    -supplemental to previous commit

--- svn/svn.sh
+++ svn/svn.sh
@@ -18,79 +18,79 @@ case "$option" in
     ;;
 
   "add-repo")
-  target=$1
-  if [ "$(echo "$target" | awk '{print substr($0, length($0))}')" = "/" ]; then
-    target=$(echo "$target" | awk '{print substr($0, 1, length($0) - 1)}')
-  fi
-
-  repo=$(echo "$target" | sed 's/.*\/\(.*\)/\1/')
-
-  if ! [ -d $target ]; then
-    mkdir -p $target
-    if [ $? -ne 0 ]; then
-      echo cannot create $target
-      exit 1
+    target=$1
+    if [ "$(echo "$target" | awk '{print substr($0, length($0))}')" = "/" ]; then
+      target=$(echo "$target" | awk '{print substr($0, 1, length($0) - 1)}')
     fi
-  fi
 
-  if [ "$(ls -A $target)" ]; then
-    echo -n "repo path is not empty, delete contents of $target? [y/n]"
-    declare result
-    while read -es -n 1 result ; do
-      if [ "$result" = "y" ]; then
-        break
-      elif [ "$result" = "n" ]; then
+    repo=$(echo "$target" | sed 's/.*\/\(.*\)/\1/')
+
+    if ! [ -d $target ]; then
+      mkdir -p $target
+      if [ $? -ne 0 ]; then
+        echo cannot create $target
         exit 1
       fi
-    done
-    if [ "$result" = "y" ]; then
-      rm -R $target/*
-    else
-      exit 1
     fi
-  fi
 
-  # cd necessary as svnadmin doesn't handle relative paths
-  cwd=$(pwd)
-  cd $target
-  svnadmin create --fs-type fsfs $target
-  chown -R $REPO_OWNER_ID:$REPO_OWNER_ID $target
-  chmod -R ug+rw $target
-
-  rm -rf temp
-  svn co $SERVER$repo temp
-  svn mkdir temp/branches temp/tags temp/trunk
-  svn ci -m "[add] repository structure" ./temp
-  rm -rf temp
-    ;;
-
-  "clean-repo")
-  target=$1
-  if ! [ -d $target ]; then
-    echo "'$target' is not a directory"
-    exit 1
-  fi
-  matches=($(find $target -name *.svn))
-  if [ ${#matches[@]} -eq 0 ]; then
-    echo "no '.svn' directories found under specified workspace"
-  else
-    for d in ${matches[@]}; do
-      echo "found a '.svn' directory at '$d'"
-    done
-    echo -n "remove all? [y/n]"
-    read -es -n1 result
-    if [ "$result" = "y" ]; then
-      for d in ${matches[@]}; do
-        rm -rf $d
-        if [[ $? -eq 0 && ! -d $d ]]; then
-          echo "removed '$d'"
-        else
-          echo "failed to remove '$d'"
+    if [ "$(ls -A $target)" ]; then
+      echo -n "repo path is not empty, delete contents of $target? [y/n]"
+      declare result
+      while read -es -n 1 result ; do
+        if [ "$result" = "y" ]; then
+          break
+        elif [ "$result" = "n" ]; then
           exit 1
         fi
       done
+      if [ "$result" = "y" ]; then
+        rm -R $target/*
+      else
+        exit 1
+      fi
     fi
-  fi
+
+    # cd necessary as svnadmin doesn't handle relative paths
+    cwd=$(pwd)
+    cd $target
+    svnadmin create --fs-type fsfs $target
+    chown -R $REPO_OWNER_ID:$REPO_OWNER_ID $target
+    chmod -R ug+rw $target
+
+    rm -rf temp
+    svn co $SERVER$repo temp
+    svn mkdir temp/branches temp/tags temp/trunk
+    svn ci -m "[add] repository structure" ./temp
+    rm -rf temp
+    ;;
+
+  "clean-repo")
+    target=$1
+    if ! [ -d $target ]; then
+      echo "'$target' is not a directory"
+      exit 1
+    fi
+    matches=($(find $target -name *.svn))
+    if [ ${#matches[@]} -eq 0 ]; then
+      echo "no '.svn' directories found under specified workspace"
+    else
+      for d in ${matches[@]}; do
+        echo "found a '.svn' directory at '$d'"
+      done
+      echo -n "remove all? [y/n]"
+      read -es -n1 result
+      if [ "$result" = "y" ]; then
+        for d in ${matches[@]}; do
+          rm -rf $d
+          if [[ $? -eq 0 && ! -d $d ]]; then
+            echo "removed '$d'"
+          else
+            echo "failed to remove '$d'"
+            exit 1
+          fi
+        done
+      fi
+    fi
     ;;
 
   "amend")
@@ -99,28 +99,28 @@ case "$option" in
     ;;
 
   "ignore")
-  if [ ! -d $(pwd)/.svn ]; then
-    echo $(pwd) is not under source control
-    exit 1
-  fi
-
-  declare list
-  for arg in $@; do
-    if [ ${#list} -eq 0 ]; then
-      list="$arg"
-    else
-      list=$list\$\'\\n\'$arg
+    if [ ! -d $(pwd)/.svn ]; then
+      echo $(pwd) is not under source control
+      exit 1
     fi
-  done
-  eval "svn propset svn:ignore $list ."
 
-  sleep 1
-  echo "svn:ignore set:"
-  svn propget svn:ignore
+    declare list
+    for arg in $@; do
+      if [ ${#list} -eq 0 ]; then
+        list="$arg"
+      else
+        list=$list\$\'\\n\'$arg
+      fi
+    done
+    eval "svn propset svn:ignore $list ."
+
+    sleep 1
+    echo "svn:ignore set:"
+    svn propget svn:ignore
     ;;
 
   *)
-  echo "unsupported option '$option'"
+    echo "unsupported option '$option'"
     ;;
 
 esac
