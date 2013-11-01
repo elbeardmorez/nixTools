17db0cd1de9309312a09a42d7606b7552024c8cd
Pete Beardmore <pete.beardmore@msn.com>
2013-11-01 09:03:26 +0000

    [add] devel.sh, c file syntax fixing
    -add 'fix' option to convert tabs and remove trailing whitespace
    -add 'find' option to identify files with tab, whitespace and brace
    position 'issues'

--- devel/devel.sh
+++ devel/devel.sh
@@ -1,6 +1,53 @@
 #!/bin/sh
 SCRIPTNAME="${0##*/}"
+IFSORG=$IFS
+TEST=${TEST:-0}
 
+function process()
+{
+  option="$1" && shift
+  case "$option" in
+    "find")
+      target="$1" && shift
+      [ ! -e "$target" ] &&  echo "[error] invalid target file/dir: '$target'" && exit 1
+      file="$1"
+      echo -e "\n##########"
+      echo -e "#searching for 'braces after new line' in file '$file'\n"
+      sed -n 'H;x;/.*)\s*\n\+\s*{.*/p' "$file"
+      echo -e "\n##########"
+      echo -e "#searching for 'tab' character' in file '$file'\n"
+      sed -n 's/\t/<TAB>/gp' "$file"
+      echo -e "\n##########"
+      echo -e "#searching for 'trailing white-space' in file '$file'\n"
+      IFS=$'\n'; lines=$(sed -n '/\s$/p' "$file"); IFS=$IFSORG
+      for line in "${lines[@]}"; do
+        echo "$line" | sed -n ':1;s/^\(.*\S\)\s\(\s*$\)/\1·\2/;t1;p'
+      done
+      ;;
+
+    "fix")
+      target="$1" && shift
+      [ ! -e "$target" ] &&  echo "[error] invalid target file/dir: '$target'" && exit 1
+      filter=".*" && [ $# -gt 0 ] && filter="$1" && shift
+      sFiles=()
+      IFS=$'\n'
+      if [ -f "$target" ]; then
+        sFiles=("$target")
+      elif [ -d "$target" ]; then
+        sFiles=($(find "$target" -iregex "$filter"))
+      fi
+      IFS="$IFSORG"
+
+      SEDCMD="$([ $TEST -gt 0 ] && echo "echo ")sed"
+      for f in "${sFiles[@]}"; do
+        # replace tabs with double spaces
+        $SEDCMD -i 's/\t/  /g' "$f"
+        # remove trailing whitespace
+        $SEDCMD -i 's/\s*$//g' "$f"
+      done
+      ;;
+
+    "debug")
 LANGUAGEDEFAULT=c
 declare -A type
 declare -A typeargs
@@ -46,3 +93,10 @@ done
 echo ""
 
 $DEBUGGER $DEBUGGERARGS
+      ;;
+  esac
+}
+
+[ $# -lt 2 ] && echo "[error] arguments 'option' 'option-arg1 ['option-arg2 .. ]" && exit 1
+option="$1" && shift
+process "$option" "$@"
