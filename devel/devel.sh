8538a9405f85606cac2bc8d0eeb5205b64d494dd
Pete Beardmore <pete.beardmore@msn.com>
2013-11-01 09:05:15 +0000

    [mod] devel.sh, whitespace changes only
    -supplemental to previous commit

--- devel/devel.sh
+++ devel/devel.sh
@@ -48,51 +48,51 @@ function process()
       ;;
 
     "debug")
-LANGUAGEDEFAULT=c
-declare -A type
-declare -A typeargs
+      LANGUAGEDEFAULT=c
+      declare -A type
+      declare -A typeargs
 
-type["c"]="gdb"
-typeargs["c"]="\$NAME --pid=\$PID"
+      type["c"]="gdb"
+      typeargs["c"]="\$NAME --pid=\$PID"
 
-NAME="$1"
-PID="${PID:-$2}"
+      NAME="$1"
+      PID="${PID:-$2}"
 
-LANGUAGE=${LANGUAGE:-$LANGUAGEDEFAULT}
+      LANGUAGE=${LANGUAGE:-$LANGUAGEDEFAULT}
 
-DEBUGGER="${type["$LANGUAGE"]}"
-DEBUGGERARGS="${typeargs["$LANGUAGE"]}"
+      DEBUGGER="${type["$LANGUAGE"]}"
+      DEBUGGERARGS="${typeargs["$LANGUAGE"]}"
 
-arrDynamic=("NAME" "PID")
-for arg in "${arrDynamic[@]}"; do
-  case "$arg" in
-    "PID")
-      PID=${PID:-$(pgrep -x "$NAME")}
-      if [ "x$PID" != "x" ]; then
-        DEBUGGERARGS=$(echo "$DEBUGGERARGS" | sed 's|\$'$arg'|'$PID'|')
-      else
-        DEBUGGERARGS=$(echo "$DEBUGGERARGS" | sed 's|--pid=\$PID||')
-      fi
-      ;;
-    *) DEBUGGERARGS=$(echo "$DEBUGGERARGS" | sed 's|\$'$arg'|'${!arg}'|') ;;
-  esac
-done
+      arrDynamic=("NAME" "PID")
+      for arg in "${arrDynamic[@]}"; do
+        case "$arg" in
+          "PID")
+            PID=${PID:-$(pgrep -x "$NAME")}
+            if [ "x$PID" != "x" ]; then
+              DEBUGGERARGS=$(echo "$DEBUGGERARGS" | sed 's|\$'$arg'|'$PID'|')
+            else
+              DEBUGGERARGS=$(echo "$DEBUGGERARGS" | sed 's|--pid=\$PID||')
+            fi
+            ;;
+          *) DEBUGGERARGS=$(echo "$DEBUGGERARGS" | sed 's|\$'$arg'|'${!arg}'|') ;;
+        esac
+      done
 
-#execute
-echo -n "[user] debug: $DEBUGGER $DEBUGGERARGS ? [(y)es/(n)o]:  "
-bRetry=1
-while [ $bRetry -eq 1 ]; do
-  echo -en '\033[1D\033[K'
-  read -n 1 -s result
-  case "$result" in
-    "n" | "N") echo -n $result; bRetry=0; echo ""; exit ;;
-    "y" | "Y") echo -n $result; bRetry=0 ;;
-    *) echo -n " " 1>&2
-  esac
-done
-echo ""
+      #execute
+      echo -n "[user] debug: $DEBUGGER $DEBUGGERARGS ? [(y)es/(n)o]:  "
+      bRetry=1
+      while [ $bRetry -eq 1 ]; do
+        echo -en '\033[1D\033[K'
+        read -n 1 -s result
+        case "$result" in
+          "n" | "N") echo -n $result; bRetry=0; echo ""; exit ;;
+          "y" | "Y") echo -n $result; bRetry=0 ;;
+          *) echo -n " " 1>&2
+        esac
+      done
+      echo ""
 
-$DEBUGGER $DEBUGGERARGS
+      $DEBUGGER $DEBUGGERARGS
       ;;
   esac
 }
