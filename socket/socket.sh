97cc5a95c6c649d5efeb6cd29c083dc7560ddd2e
Pete Beardmore <pete.beardmore@msn.com>
2019-01-02 07:52:48 +0000

    [mod] socket.sh, refactor server side code
    -add persistent mode switch and close connection on receipt or
    timeout if this switch is not specified

--- socket/socket.sh
+++ socket/socket.sh
@@ -1,27 +1,43 @@
 #!/bin/sh
 
 SERVER=${SERVER:-localhost}
+SERVER_TIMEOUT=${SERVER_TIMEOUT:-60}
 PORT=${PORT:-666}
 
 direction="$1" && shift
 
 case "$direction" in
   "in")
-    echo [user] socket opened
+    # server side
+    args=("--listen" "--local-port=$PORT")
+    persistent=0
+    if [ "$#" -gt 0 ]; then
+      while [ -n "$1" ]; do
+        case "$1" in
+          "-ps"|"--persistent") persistent=1 ;;
+          *) echo "[user] unrecognised option arg '$1', dropped"
+        esac
+        shift
+      done
+    fi
+    [ $persistent -eq 0 ] && args[${#args}]="--wait=$SERVER_TIMEOUT"
+    echo [info]$([ $persistent -eq 1 ] && echo " persistent") socket opened
     while [ 1 == 1 ]; do
-      nc -l -p $PORT -w 60 > socket
+      nc "${args[@]}" > socket
       size=`du -h socket | cut -d'	' -f1`
       if [ "$size" != "0" ]; then
-        echo "$size bytes dumped"
+        echo "[info] $size bytes dumped"
         mv socket data
+        [ $persistent -eq 0 ] && exit 0
       else
-        echo [user[ socket closed
+        # non persistent mode only
+        echo [info[ socket closed
         exit 0
       fi
-      sleep 2
     done
     ;;
   "out")
+    # client side
     file="$1"
     [ ! -f "$file" ] && echo "[error] no push data" && exit 1
     nc -c $SERVER $PORT < "$file"
