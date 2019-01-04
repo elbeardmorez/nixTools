4470d63ab36b5f503b5c82b8f762d1b50b9cce6f
Pete Beardmore <pete.beardmore@msn.com>
2019-01-04 09:55:56 +0000

    [add] socket.sh, help function

--- socket/socket.sh
+++ socket/socket.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+SCRIPTNAME=${0##*/}
+
 # compatibility
 if [ -n "$BASH_VERSION" ]; then
   CMDARGS_READ_SINGLECHAR=("-s" "-n1")
@@ -12,6 +14,42 @@ SERVER=${SERVER:-localhost}
 SERVER_TIMEOUT=${SERVER_TIMEOUT:-60}
 PORT=${PORT:-666}
 RETRIES=10
+
+help() {
+  echo -e "
+SYNTAX: $SCRIPTNAME [OPTION] [ARGS]\n
+where OPTION can be:
+  in [ARGS]  : server side socket setup for netcat
+    where ARGS can be:
+    -ps, --persistent  : leave connection open by default the
+                         script exits after the first payload is
+                         received
+    -nc, --noclean  : don't even ask to delete existing blobs in
+                      current directory
+
+  out [ARGS] [DATA [DATA2 ..]]  : (default) client side socket
+                                  setup for netcat
+    where ARGS can be:
+
+    -s, --server ADDRESS  : specify the target server address
+                            (default: localhost)
+    -r, --retries COUNT  : set the number retries when a push fails
+                           (default: 10)
+    -a, --any  : process non-file/dir args as valid raw data strings
+                 to be push to the server
+
+  and DATA args are either file paths or raw data (where '--any'
+  switch is given)
+
+  environment variables:
+  'SERVER' (client)  : as detailed above
+  'PORT' (server / client)  : set communication port (default: 666)
+  'SERVER_TIMEOUT' (server)  : in non-persistent mode, the server
+                               side will automatically terminate after
+                               this interval where no packets are
+                               received (default: 60)
+"
+}
 
 fnNextFile() {
   file=$1
@@ -143,7 +181,8 @@ fnProcess() {
 }
 
 option="out"
-[[ "x$(echo "$1" | sed -n '/\(in\|out\)/p')" != "x" ]] && option="$1" && shift
+[[ "x$(echo "$1" | sed -n '/\(in\|out\|help\)/p')" != "x" ]] && option="$1" && shift
 case $option in
   "in"|"out") fnProcess $option "$@" ;;
+  "help") help ;;
 esac
