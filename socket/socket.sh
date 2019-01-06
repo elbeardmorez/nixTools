217a79725116efc82ba2a155045c1f356f5e5b7d
Pete Beardmore <pete.beardmore@msn.com>
2019-01-06 08:20:19 +0000

    [mod] socket.sh, clean up on exit
    -remove socket file

--- socket/socket.sh
+++ socket/socket.sh
@@ -157,8 +157,8 @@ fnProcess() {
           fi
           [ $persistent -eq 0 ] && exit 0
         else
-          # non persistent mode only
-          echo "[info] socket closed"
+          echo "[info] socket closed, removing socket"
+          [ -f socket ] && rm socket
           exit 0
         fi
       done
