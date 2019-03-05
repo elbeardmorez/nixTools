a16b4cf7349a7d91ef872b2da55ab2fdfe0c428e
Pete Beardmore <pete.beardmore@msn.com>
2019-03-05 11:16:37 +0000

    [fix] blog.sh, mod file movements
    -correct intention

--- blog/blog.sh
+++ blog/blog.sh
@@ -173,9 +173,9 @@ case "$option" in
       [ ${#matches[@]} -ne 1 ] && echo "[error] couldn't find unique blog entry using search term '$search'" && exit 1
       echo "[info] targetting matched entry '${match[0]}'"
       # move original
-      mv "${matches[0]}" "$f_entry.tmp"
+      mv "${matches[0]}" "$f_entry"
       # edit temp version
-      cp "${matches[0]}" "$f_entry.tmp"
+      cp "$f_entry" "$f_entry.tmp"
     else
       echo "[info] targetting unpublished entry'"
     fi
