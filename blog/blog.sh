51ff8376b8134481b1edd1e32bd2d8f86324a22d
Pete Beardmore <pete.beardmore@msn.com>
2019-03-06 10:41:26 +0000

    [add] blog.sh, highlight real modified timestamps
    -note that 'column' calculates column width based on length including
    escape characters, hence the hack adding highlight on and off without
    wrapping any actual chars just to even up ultimate column widths for
    highlighted / non-highlighted

--- blog/blog.sh
+++ blog/blog.sh
@@ -215,9 +215,8 @@ case "$option" in
     for f in "${files[@]}"; do
       dt_created="$(sed -n 's/'\''date created'\'':[ ]*'\''\(.*\)'\''$/\1/p' "$f")"
       dt_modified="$(sed -n 's/'\''date modified'\'':[ ]*'\''\(.*\)'\''$/\1/p' "$f")"
-      [ -z "$dt_modified" ] && dt_modified="$dt_created"
       title="$(sed -n 's/'\''title'\'':[ ]*'\''\(.*\)'\''$/\1/p' "$f")"
-      tb+="\n[$l]\t$c_red$title$c_off\t$dt_created\t$dt_modified\t$f"
+      tb+="\n[$l]\t$c_red$title$c_off\t$dt_created\t$([ -n "$dt_modified" ] && echo "$c_bld$dt_modified$c_off" || echo "$c_bld$c_off$dt_created")\t$f"
       l=$(($l+1))
     done
     echo "# published entries"
