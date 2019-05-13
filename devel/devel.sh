10d9e421e2142c700e71346aff756c7e8c3549be
Pete Beardmore <pete.beardmore@msn.com>
2019-05-13 08:58:26 +0100

    [mod] devel.sh, refactor, braces transform
    -deal with single line files

--- devel/devel.sh
+++ devel/devel.sh
@@ -539,7 +539,7 @@ fn_refactor() {
             "braces")
               # remove new line preceding function brace
               fn_header "> removing 'new lines preceding function braces' in file '$f'"
-              $sedcmd -i -n '1{h;b};/^\s*{/{x;/)\s*$/{x;H;x;s/\s*\n\s*/ /;p;n;x;b;};x;};x;p;' "$f"
+              $sedcmd -i -n '1{${p;b;};h;b};/^\s*{/{x;/)\s*$/{x;H;x;s/\s*\n\s*/ /;p;n;x;b;};x;};x;p;' "$f"
               ;;
             "tabs")
               # replace tabs with double spaces
