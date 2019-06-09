d767a41f6863177d7c495bde9abb893e482b4c93
Pete Beardmore <pete.beardmore@msn.com>
2019-06-09 14:59:04 +0100

    [mod] math.sh, refactor
    -syntax improvements
    -protect underquoted returns
    -string format harmonisation
    -cleanup code 'section' labels, add missing

--- math/math.sh
+++ math/math.sh
@@ -15,30 +15,41 @@ max="define max(x,y) { if(x>y) return x else return y}; scale=0"
 min="define min(x,y) { if(x<y) return x else return y}; scale=0"
 factorial="define factorial(n) { if (n == 0) return(1); return(n * factorial(n - 1)); }"
 
-# params
-[ ! $# -gt 0 ] && echo error: not enough args && exit 1
-funcs=
+# process args
+declare exp
+declare funcs
+declare base
+declare scale
+declare case_
+
+[ $# -eq 0 ] && \
+  echo "[error] not enough args" && exit 1
 
 # base convertion
-#echo "16 i 2 o C2 p" | dc
-#echo "ibase=16; obase=2; C2" | bc
-base=
-[ $# -gt 0 ] && [ "x$1" = "xh2b" ] && base="obase=2; ibase=16" && shift && scale="" && bCase=1
-[ $# -gt 0 ] && [ "x$1" = "xb2h" ] && base="obase=16; ibase=2" && shift && scale=""
-[ $# -gt 0 ] && [ "x$1" = "xh2d" ] && base="ibase=16" && shift && scale="" && bCase=1
-[ $# -gt 0 ] && [ "x$1" = "xd2h" ] && base="obase=16" && shift && scale=""
-[ $# -gt 0 ] && [ "x$1" = "xd2b" ] && base="obase=2" && shift && scale=""
-[ $# -gt 0 ] && [ "x$1" = "xb2d" ] && base="ibase=2" && shift && scale=""
+if [ $# -gt 1 ]; then
+  case "$1" in
+    "h2b") base="obase=2; ibase=16" && shift && scale="" && case_=1 ;;
+    "b2h") base="obase=16; ibase=2" && shift && scale="" ;;
+    "h2d") base="ibase=16" && shift && scale="" && case_=1 ;;
+    "d2h") base="obase=16" && shift && scale="" ;;
+    "d2b") base="obase=2" && shift && scale="" ;;
+    "b2d") base="ibase=2" && shift && scale="" ;;
+  esac
+fi
 
-# parse expression for bash variables. don't attempt global variable
-# replacement, functions yes
-exp="$1"
+# expression
+exp="$1" && shift
+
+# scale
+[ $# -gt 0 ] && scale=$1 && shift
+
+# parse expression for functions and shell variables
 l=1
-while [ "x$(echo "$exp" | sed -n '/[^\]*\$/p')" != "x" ]; do
+while [ -n "$(echo "$exp" | sed -n '/[^\]*\$/p')" ]; do
   var=$(echo "$exp" | sed -n 's|^[^\]*\$\([a-zA-Z0-9_]\+\).*$|\1|p')
   var2="${!var}"
-  if [[ ${#var2} -ge 6 && "${var2:0:6}" == "define" ]]; then
-    # add to function list and strip all instances
+  if [[ ${#var2} -ge 6 && "x${var2:0:6}" == "xdefine" ]]; then
+    # add to function definition string and replace all instances
     funcs=$(echo -e "$funcs$var2\n")
     exp=$(echo "$exp" | sed 's|'\$$var'|'$var'|g')
   else
@@ -46,29 +57,30 @@ while [ "x$(echo "$exp" | sed -n '/[^\]*\$/p')" != "x" ]; do
     exp=$(echo "$exp" | sed 's|'\$$var'|'$var2'|')
   fi
 done
-shift
-[ $# -gt 0 ] && scale=$1 && shift
 
+# units
 unit=$(echo "$exp" | sed -n 's/.*\(£\|\$\|k\).*/\1/p')
-if [ ! "$unit" = "x" ]; then exp=$(echo "$exp" | sed 's/\(\\£\|\\\$\|\\k\)//g' | sed 's/\(£\|\$\|k\)//g'); fi
-[[ -n "$bCase" && $bCase -eq 1 ]] && exp="$(echo "$exp" | awk '{print toupper($0);}')"
+[ -n "$unit" ] && \
+  exp="$(echo "$exp" | sed 's/\(\\£\|\\\$\|\\k\)//g' | sed 's/\(£\|\$\|k\)//g')"
+[[ -n "$case_" && $case_ -eq 1 ]] && \
+  exp="$(echo "$exp" | awk '{print toupper($0);}')"
 
-# add required function definitions
+# prefix function definitions
 [ "$funcs" ] && exp="$funcs; $exp"
-# add required base conversion
+# prefix base conversion
 [ "$base" ] &&  exp="$base; $exp"
 
-# generic fixes for expressions. remove superfluous '+'
-exp=$(echo "$exp" | sed 's/^+\|\((\)+/\1/g')
+# generic mods
+## remove superfluous '+'
+exp="$(echo "$exp" | sed 's/^+\|\((\)+/\1/g')"
 
 # calc
-res=$(echo -e "$exp" | bc -l)
+res="$(echo -e "$exp" | bc -l)"
 
 # override scale?
 scale2="$(echo "$exp" | sed -n 's|^.*scale=\([0-9]\+\).*$|\1|p')"
 [ "x$scale2" != "x" ] && scale=$scale2
-
-# log
 [ $DEBUG -gt 0 ] && echo "[debug] scale: '$scale', exp: '$exp', unit: '$unit'"
 
-[ -n "$scale" ] && echo $unit$(echo "scale=${scale:-2};$res/1" | bc) || echo "$res"
+# result
+[ -n "$scale" ] && echo "$unit$(echo "scale=${scale:-2};$res/1" | bc)" || echo "$res"
