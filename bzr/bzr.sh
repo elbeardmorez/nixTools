64a28f385ebb1c3276e01729d2b97e64c3f87f2d
Pete Beardmore <pete.beardmore@msn.com>
2019-08-24 12:06:14 +0100

    [mod] bzr.sh, improve help
    -add missing help entries
    -improve formatting

--- bzr/bzr.sh
+++ bzr/bzr.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+SCRIPTNAME=${0##*/}
 
 DEBUG=${DEBUG:-0}
 TEST=${TEST:-0}
@@ -7,22 +8,37 @@ IFSORG="$IFS"
 maxmessagelength=150
 
 function help() {
-  echo "SYNTAX: bzr.sh [OPTION]
-  with OPTION:
-    log X  : output log information for commit(s) X:
-      with supported X:
-        [X : 1 <= X <= 25]  : last x commits    : -r-X..
-        [X : X > 25]        : revision X        : -rX
-        [+X : 1 <= X <= ..] : revision X        : -rX
-        [rX : 1 <= X <= ..] : revision X        : -rX
-        [-X : X <= -1]      : last X commits    : -r-X..
-        [rX_1 rX_2 : 1 <= X_1 <= .., 1 <= X_2 <= ..]
-          : commits between revision X_1 and revision X_2 inclusive
-          : -r'min(X_1,X_2)'..'max(X_1,X_2)'
-        [-X_1 -X_2 : 1 <= X_1 <= .., 1 <= X_2 <= ..]
-          : commits between revision 'HEAD - X_1' and revision
-            'HEAD - X_2' inclusive
-          : -r'-min(X_1,X_2)'..-'max(X_1,X_2)'
+  echo -e "SYNTAX: $SCRIPTNAME [OPTION]
+\nwith OPTION:
+\n  log [r]X [[r]X2]  : output log information for commit(s) X:
+\n    with supported X:
+      [X : 1 <= X <= 25]  : last x commits (-r-X..)
+      [X : X > 25]        : revision X (-rX)
+      [+X : 1 <= X <= ..] : revision X (-rX)
+      [rX : 1 <= X <= ..] : revision X (-rX)
+      [-X : X <= -1]      : last X commits (-r-X..)
+      [rX_1 rX_2 : 1 <= X_1/X_2 <= ..]
+        : commits between revision X_1 and revision X_2 inclusive
+          (-r'min(X_1,X_2)'..'max(X_1,X_2)')
+      [-X_1 -X_2 : 1 <= X_1/X_2 <= ..]
+        : commits between revision 'HEAD - X_1' and revision
+          'HEAD - X_2' inclusive (-r'-min(X_1,X_2)'..-'max(X_1,X_2)')
+\n  diff [REVISION]  : show diff of REVISION again previous
+                     (default: HEAD)
+  patch [REVISION] [TARGET]  : format REVISION (default: HEAD) as a
+                               diff patch file with additional
+                               context information inserted as a
+                               header to TARGET (default: auto-
+                               generated from commit message)
+  commits [SEARCH] [TYPE]  : search log for commits containing SEARCH
+                             in TYPE field
+\n    with support TYPE:
+      message  : search the message content (default)
+      author  : search the author field
+      committer  : search the commiter field
+\n  commits-dump TARGET [SEARCH] [TYPE]
+    : wrapper for 'commits' which also dumps any matched commits
+      to TARGET
 "
 }
 
